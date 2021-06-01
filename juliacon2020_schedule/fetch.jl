f423db028e081d5efcbc969f03052c7416d020ca
Carsten Bauer <crstnbr@gmail.com>
2021-06-01 22:43:27 +0200

    test

--- juliacon2020_schedule/fetch.jl
+++ juliacon2020_schedule/fetch.jl
@@ -1,5 +1,3 @@
-using Downloads
-
 UPSTREAM_URL = "https://pretalx.com/juliacon2020/schedule/export/schedule.json"
 
-fp = Download.download(UPSTREAM_URL, "schedule.json")
+fp = download(UPSTREAM_URL, "schedule.json")
